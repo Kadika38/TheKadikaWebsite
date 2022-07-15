f5ba945a78d8e7be1955f02d1ede7d8e7991168e
Kadika <dryan10101@gmail.com>
2022-07-15 12:06:21 -0700

    basic navbar on landing page html and css

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    maxAge: 1800000 //half an hour
+    /* maxAge: 1800000 //half an hour */
   },
   resave: false,
   saveUninitialized: true,
