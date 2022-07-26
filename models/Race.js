6ec4f86705ebdc38e0ef1f94a1d157c5355a3008
Kadika <dryan10101@gmail.com>
2022-07-26 10:00:24 -0700

    added Race model

--- models/Race.js
+++ models/Race.js
@@ -1,1 +1,35 @@
 //references Things and Choices
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Race extends Model {}
+
+Race.init(
+    {
+        //id
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        info: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'race',
+    }
+);
+
+module.exports = Race;
