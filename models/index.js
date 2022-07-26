9032fde7003739e3ab1728c19ec73264622122b6
Kadika <dryan10101@gmail.com>
2022-07-26 10:55:00 -0700

    added new models to index without reference definitions

--- models/index.js
+++ models/index.js
@@ -1,7 +1,17 @@
 const User = require('./User');
 const Character = require('./Character');
+const Choice = require('./Choice');
+const Feat = require('./Feat');
+const Option = require('./Option');
+const Race = require('./Race');
+const Thing = require('./Thing');
 
 module.exports = {
     User,
     Character,
+    Choice,
+    Feat,
+    Option,
+    Race,
+    Thing,
 };
