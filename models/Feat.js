6efe821124b5b8457ca028319344f719d8e6961a
Kadika <dryan10101@gmail.com>
2022-07-26 10:33:58 -0700

    added Feat Model

---
+++ models/Feat.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Feat extends Model {}
+
+Feat.init(
+    {
+        //id
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        info: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'feat',
+    }
+);
+
+module.exports = Feat;
