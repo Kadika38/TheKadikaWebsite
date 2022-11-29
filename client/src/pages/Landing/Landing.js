dad97a60742094602d50bff45af31ddc016d09bb
Kadika <dryan10101@gmail.com>
2022-11-29 15:55:29 -0800

    removed some old files and adjusted others for pre-landing page file structure

---
+++ client/src/pages/Landing/Landing.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import "./style.css";
+
+export default function Landing() {
+
+    //this is the landing page
+    //login/signup links
+    //build a character links
+    //more to come
+    //leave space for ads
+    return (
+        <div>
+            
+        </div>
+    );
+};
--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,19 +1,11 @@
 import React, { useState } from "react";
 import "./style.css";
-import Board from "../../components/Board/Board";
 
 export default function Main() {
 
     return (
         <div className="mainContainer">
-            <div className="subContainer">
-                <div className="column">
-                    <Board></Board>
-                </div>
-                <div className="column">
-                    <Board></Board>
-                </div>
-            </div>
+            <Landing></Landing>
         </div>
     );
 };
