4eb574c4780b193bb15417ca1e692439bd1dd140
Kadika <dryan10101@gmail.com>
2022-12-01 18:00:04 -0800

    changed comments on Landing.js and added Builder empty files

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -4,10 +4,7 @@ import "./style.css";
 export default function Landing() {
 
     //this is the landing page
-    //login/signup links
-    //build a character links
-    //more to come
-    //leave space for ads
+    //idk what goes here
     return (
         <div>
             
